918b790e3f13625098faca06260fb128bd05e63b
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-12 12:58:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
-// Menu Hamburger (Mobile)
 function toggleMenu(btn) {
   btn.classList.toggle("open");
   document.getElementById("mobileMenu").classList.toggle("hidden");
 }
 
-// Mudança do nome da marca ao rolar a página
 window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
   const brand = document.getElementById("brand");
   if (window.scrollY > 50) {
     brand.textContent = "ISB";
