cd0c041a71c02b03fe8e62186ff44ba00b711e0d
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-12 12:16:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// Menu Hamburger (Mobile)
+function toggleMenu(btn) {
+  btn.classList.toggle("open");
+  document.getElementById("mobileMenu").classList.toggle("hidden");
+}
+
+// Mudança do nome da marca ao rolar a página
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  const brand = document.getElementById("brand");
+  if (window.scrollY > 50) {
+    brand.textContent = "ISB";
+  } else {
+    brand.textContent = "Igreja Satanista Brasileira";
+  }
+});
