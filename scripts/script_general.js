65309cd7287592a90e4ea9df3bc8c3505325251b
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-14 09:36:50 -0300

    Atualizar o script_general.js

--- scripts/script_general.js
+++ scripts/script_general.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const quotes = [
-  `"Tu és o altar sobre o qual tua vontade é celebrada."\n---\n— Bíblia da ISB`,
-  `"Negar o céu é afirmar a terra."\n---\n— Arumir, 2024`,
-  `"Não serviremos. E isso nos faz livres."\n---\n— Bíblia da ISB`,
-  `"A carne é sagrada pois nela habita o eu soberano."\n---\n— Bíblia da ISB`,
-  `"A dúvida é o fogo que depura a verdade."\n---\n— Cap. IV, Bíblia da ISB`,
-  `"Satã não nos guia: nos inspira a guiar-nos por nós mesmos."\n---\n— Arumir`
+  `"Tu és o altar sobre o qual tua vontade é celebrada."\n<hr class="quote-divider">\n— Bíblia da ISB`,
+  `"Negar o céu é afirmar a terra."\n<hr class="quote-divider">\n— Arumir, 2024`,
+  `"Não serviremos. E isso nos faz livres."\n<hr class="quote-divider">\n— Bíblia da ISB`,
+  `"A carne é sagrada pois nela habita o eu soberano."\n<hr class="quote-divider">\n— Bíblia da ISB`,
+  `"A dúvida é o fogo que depura a verdade."\n<hr class="quote-divider">\n— Cap. IV, Bíblia da ISB`,
+  `"Satã não nos guia: nos inspira a guiar-nos por nós mesmos."\n<hr class="quote-divider">\n— Arumir`
 ];
 
   let current = 0;
