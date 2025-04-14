7c246c7b2486b18cdbd316bc42024f7303bbf888
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-14 10:26:15 -0300

    Atualizar o script_general.js

--- scripts/script_general.js
+++ scripts/script_general.js
@@ -66,3 +66,18 @@ function updateQuote() {
 }
 
 setInterval(updateQuote, 7000); // troca a cada 7s
+
+// Carrega navbar e footer automaticamente
+window.addEventListener("DOMContentLoaded", () => {
+  fetch('/partials/navbar.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('navbar-placeholder').innerHTML = data;
+    });
+
+  fetch('/partials/footer.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    });
+});
