819e3c6b22aeeb9b18aa1726624ae5ce9cc578c6
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-15 00:59:57 -0300

    Atualizar o navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+!-- document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menuToggle");
   const menu = document.getElementById("navbarMenu");
 
   toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
   });
-});
+}); --
