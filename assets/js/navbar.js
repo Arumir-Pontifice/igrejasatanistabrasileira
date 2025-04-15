44fb8596fce089417bffab47176b266640c3b36e
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-15 07:42:37 -0300

    Atualizar o navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("menuToggle");
+  const navbarMenu = document.getElementById("navbarMenu");
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.getElementById('menuToggle');
-  const navbarMenu = document.getElementById('navbarMenu');
-
-  toggleBtn.addEventListener('click', () => {
-    toggleBtn.classList.toggle('activate');
-    navbarMenu.classList.toggle('show');
+  document.addEventListener("click", function (event) {
+    if (
+      navbarMenu.classList.contains("show") &&
+      !event.target.closest(".navbar") &&
+      !event.target.closest("#menuToggle")
+    ) {
+      navbarMenu.classList.remove("show");
+      toggleBtn.classList.remove("active");
+    }
   });
+
+  window.addEventListener("scroll", function () {
+    navbarMenu.classList.remove("show");
+    toggleBtn.classList.remove("active");
+  });
+
+  if (toggleBtn && navbarMenu) {
+    toggleBtn.addEventListener("click", function (event) {
+      event.stopPropagation();
+      toggleBtn.classList.toggle("active");
+      navbarMenu.classList.toggle("show");
+    });
+  }
 });
-
-
-
-
-
