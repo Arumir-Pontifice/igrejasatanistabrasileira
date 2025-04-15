f24e76c4f50b9b6aad0d63007968752d9f659b8f
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-14 21:57:37 -0300

    Criar load-navbar.js

---
+++ scripts/load-navbar.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("navbar-placeholder").innerHTML = data;
+
+      // Agora que a navbar foi carregada, ative o menu
+      const toggle = document.getElementById("menuToggle");
+      const menu = document.getElementById("navbarMenu");
+
+      if (toggle && menu) {
+        toggle.addEventListener("click", () => {
+          menu.classList.toggle("active");
+        });
+      }
+    });
+});
