c52a6fcfb8e65e2f81eb7ceff71a304bf07c29f2
Arumir-Pontifice <igrejasatanistabrasileira.isb@gmail.com>
2025-04-15 21:56:03 -0300

    Delete assets directory

--- assets/js/footer.js
+++
@@ -1,8 +0,0 @@
-// Injetar footer
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/footer.html")
-    .then((res) => res.text())
-    .then((html) => {
-      document.getElementById("footer-container").innerHTML = html;
-    });
-});
